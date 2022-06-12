e8e858672f025124352a63a2e5bcc808ee75917c
R-oso <luukroso@gmail.com>
2022-06-12 09:34:15 +0200

    Added Enemy class with attributes for inheritance.

--- src/Enemy.ts
+++ src/Enemy.ts
@@ -2,8 +2,8 @@ import * as PIXI from "pixi.js";
 import { Game } from "./Game";
 
 export class Enemy extends PIXI.Sprite {
-  private speed: number = 0;
-  private game: Game;
+  public speed: number = 0;
+  public game: Game;
 
   constructor(texture: PIXI.Texture, game: Game) {
     super(texture);
@@ -11,6 +11,5 @@ export class Enemy extends PIXI.Sprite {
 
   public update() {
     this.x -= this.speed;
-    if (this.x < -100) this.resetPosition();
   }
 }
