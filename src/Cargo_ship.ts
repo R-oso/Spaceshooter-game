bb2a8bce229a66f436321117f183dd2d43b7936f
R-oso <luukroso@gmail.com>
2022-06-12 09:35:20 +0200

    Cargo ship class now has specific code to go with it

--- src/Cargo_ship.ts
+++ src/Cargo_ship.ts
@@ -3,7 +3,22 @@ import { Game } from "./Game";
 import { Enemy } from "./Enemy";
 
 export class Cargo_ship extends Enemy {
-  constructor() {
-    super(texture);
+  constructor(texture: PIXI.Texture, game: Game) {
+    super(texture, game);
+
+    this.scale.set(2.3, 2.3);
+    this.x = Math.floor(Math.random() * (-600 - 400)) + -400;
+    this.y = Math.random() * game.pixi.screen.bottom;
+    this.speed = Math.random() * -6 + -1;
+  }
+
+  resetPosition() {
+    // Random number between 200 and 400 (Min and Max)
+    this.x = Math.floor(Math.random() * (-400 - -200)) + -200;
+  }
+
+  public update() {
+    this.x -= this.speed;
+    if (this.x >= 3000) this.resetPosition();
   }
 }
