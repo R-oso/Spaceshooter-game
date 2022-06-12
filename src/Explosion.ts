ff27d679d51a7a57dc90ae21d4411f46b2a9ddef
R-oso <luukroso@gmail.com>
2022-06-12 09:36:17 +0200

    Explosion class added (work in progress)

---
+++ src/Explosion.ts
@@ -0,0 +1,21 @@
+import * as PIXI from "pixi.js";
+import { Game } from "./Game";
+
+export class Explosion extends PIXI.AnimatedSprite {
+  game: Game;
+  constructor(explosionTextures: PIXI.Texture[], x: number, y: number) {
+    super(explosionTextures);
+
+    this.anchor.set(0.5);
+    this.x = x;
+    this.y = y;
+    // this.rotation = Math.random() * Math.PI
+    this.scale.set(0.4 + Math.random() * 0.5);
+    this.animationSpeed = 0.5;
+    this.loop = false;
+
+    this.onComplete = () => this.destroy();
+
+    this.play();
+  }
+}
